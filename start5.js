36d57bc0404713e33aa9ff7013591b88194e3bec
Ricardo Gonzalez <richi115@gmail.com>
2015-02-20 08:05:48 +0100

    20150220

--- start5.js
+++ start5.js
@@ -12,69 +12,71 @@ var act_jobs=[]
 var mdm_rescan=10000
 var msg_por_loop=0
 
+
+
+
+
 //async.forever(
 //	function(next){
-		async.series([
-			function(callback){
+		async.series([										//main loop
+			function(cb_mainloop){								//Paso 1 cargar modems
 				if (mdm_rescan>config.MDM_RESCAN){
 					mdm.load_mdm(function(datosmodems){
 						act_mdms=datosmodems
 						msg_por_loop=act_mdms.length * config.MSG_POR_MDM;
 						mdm_rescan=0
-						callback(null,'Cargados ' + act_mdms.length + ' modem(s)')
+						cb_mainloop(null,'Cargados ' + act_mdms.length + ' modem(s)')
 					})
 				} else {
 					mdm_rescan++;
-					callback(null,'No fue necesario reload, msg por loop: ' + msg_por_loop);
+					cb_mainloop(null,'No fue necesario reload, msg por loop: ' + msg_por_loop);
 				}
 			},
-			function(callback){
+			function(cb_mainloop){								//Paso 2 cargar jobs 
 				job.load_job(function(datosjobs){
 					act_jobs=ctrl.job_ponderar(datosjobs,msg_por_loop)
-					callback(null,'JOBS: Cargados '+ act_jobs.length + ' job(s)');
+					cb_mainloop(null,'JOBS: Cargados '+ act_jobs.length + ' job(s)');
 				})
 			},
-			function(callback){
-				async.each(act_jobs,function(jobencurso,cb_jobencurso){   //por cada job
+			function(cb_mainloop){								//Paso 3 enviar los mensajes de cada job
+				async.each(act_jobs,function(jobencurso,cb_jobloop){   //Por cada job hace este WF 
 					async.waterfall([
-						function(callback_msg){							//saco los mensajes de cada job
-							var act_msg
-							msg.load_msg(jobencurso._id,jobencurso.msg_a_enviar,function(mensajes){
-								act_msgs=mensajes
-								callback_msg(null,act_msgs)
+						function(cb_lotemsg){							//Sacar todos los mensajes de este job
+							msg.load_msg(jobencurso._id,jobencurso.msg_a_enviar,function(lotemsgs){
+								cb_lotemsg(null,lotemsgs)
 							})
 						},
-						function(msg_de_este_job,callback_msg){				//secuencia de envio
-							async.eachSeries(msg_de_este_job,function(msgencurso,cb_msgencurso){
+						function(lotemsgs,cb_lotemsg){				//Secuencia de envio
+							async.eachSeries(lotemsgs,function(msgencurso,cb_msgloop){  //Por cada msg unico hacer este WF
 								async.waterfall([                    
-									function(cb_envio){  				//buscar modem
+									function(cb_sendloop){  				//buscar modem
 										var nro_modem=parseInt(Math.random()*100)
 										console.log('MODEM ENCONTRADO')
-										cb_envio(null,nro_modem)
+										cb_sendloop(null,nro_modem)
 									},
-									function(modem_asignado,cb_envio){  //enviar mensaje y actualizar estado msg
+									function(modem_asignado,cb_sendloop){  //enviar mensaje y actualizar estado msg
 										var estado_modem=1
 										console.log('MENSAJE A: '+msgencurso.dst+' USANDO MDM: '+modem_asignado)
-										cb_envio(null,modem_asignado,estado_modem)
+										cb_sendloop(null,modem_asignado,estado_modem)
 									},
-									function(modem_asignado,estado_modem,cb_envio){ //actualizar modem
+									function(modem_asignado,estado_modem,cb_sendloop){ //actualizar modem
 										console.log('Modem '+modem_asignado+' actualizado')
-										cb_envio(null)
+										cb_sendloop(null)
 									}],
 									function(err,result_envio){
-										cb_msgencurso()
+										cb_msgloop()
 									}
 								)
 								
 							})
-							callback_msg(null,'MSGS: Cargados '+ msg_de_este_job.length + ' mensaje(s)')
+							cb_lotemsg(null,'MSGS: Cargados '+ lotemsgs.length + ' mensaje(s)')
 						}],
 						function(err,result_msg){
-							cb_jobencurso()
+							cb_jobloop()
 						}
 					)
 				})
-				callback(null)
+				cb_mainloop(null)
 			}],
 			function(err, resultados){
 				console.log(resultados[0])
